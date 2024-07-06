d27ef76fb77fee8adf6208e6f9915532353fd70e
Adriano <adriano.abbruzzese@gmail.com>
2024-07-07 01:05:50 +0200

    lista e inserimento scooter - inizio

--- rifornimento/lib/main.dart
+++ rifornimento/lib/main.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import 'package:hive/hive.dart';
-import 'FormDataRifornimento';
+import 'package:hive_flutter/adapters.dart';
+import 'package:rifornimento/ScooterBox';
+import 'models/Scooter';
 
-void main() {
-  runApp(MyApp());
+Future<void> main() async {
+  Hive.initFlutter();
+  await Hive.openBox('scooter');
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Il tuo rifornimento',
       home: MyHomePage(),
     );
@@ -18,128 +22,171 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final _formKey = GlobalKey<FormState>();
-  final _dataController = TextEditingController(
-      text: DateFormat('dd-MM-yyyy').format(DateTime.now()));
-  final _kmController = TextEditingController();
-  final _litriBenzinaController = TextEditingController();
-  final _litriOlioController = TextEditingController();
+  List<Scooter> _scooters = [];
+
+  Box box = Hive.box('scooter');
+  //get box => null;
 
   @override
-  void dispose() {
-    _dataController.dispose();
-    _kmController.dispose();
-    _litriBenzinaController.dispose();
-    _litriOlioController.dispose();
-    super.dispose();
+  void initState() {
+    super.initState();
+
+    setState(() {
+      if (box.isNotEmpty) {
+        _scooters = box.values
+            .toList()
+            .map((scooterBox) => Scooter(nome: scooterBox.nome))
+            .toList();
+      }
+      box.close();
+    });
   }
+
+  // Funzione per la selezione di uno scooter
+  void _selectScooter(Scooter scooter) {
+    // Implementa la navigazione alla schermata di dettaglio dello scooter selezionato
+    // (passa i dati dello scooter al nuovo widget)
+  }
+
+  // Funzione per la modifica di uno scooter
+  void _editScooter(Scooter scooter) {
+    // Implementa la navigazione alla schermata di modifica dello scooter
+    // (passa i dati dello scooter al nuovo widget)
+  }
+
+  // Funzione per eliminare uno scooter
+  void _deleteScooter(Scooter scooter) {
+    final box = Hive.box<ScooterBox>('scooter');
+    box.delete(ScooterBox(
+        nome: scooter.nome)); // Elimina lo scooter dalla scatola Hive
+
+    setState(() {
+      _scooters.remove(scooter);
+    });
+  }
+
+  final _nomeController = TextEditingController();
+
+  void _addScooter() async {
+    // Mostra un AlertDialog per l'inserimento dei dati
+    final result = await showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Aggiungi nuovo scooter'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: _nomeController,
+                decoration: const InputDecoration(labelText: 'Nome'),
+                autofocus: false,
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context, null),
+              child: const Text('Annulla'),
+            ),
+            TextButton(
+              onPressed: () {
+                if (_nomeController.text.isEmpty) {
+                  // Mostra un messaggio di errore se il campo è vuoto
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Compila il campo nome'),
+                    ),
+                  );
+                  return;
+                }
+                // Salva il nome nel box Hive
+                try {
+                  //final box = Hive.box<ScooterBox>('scooter');
+                  box.add(ScooterBox(nome: _nomeController.text));
+                } catch (error) {
+                  // Gestisci l'errore qui
+                  print('Errore durante l\'aggiunta dello scooter: $error');
+                  // Potresti anche mostrare un messaggio di errore all'utente
+                }
+
+                // Aggiorna la lista di scooter
+                setState(() {
+                  _scooters.add(Scooter(nome: _nomeController.text));
+                });
+
+                // Pulisci il campo del nome
+                _nomeController.clear();
+
+                // Chiudi il dialog
+                Navigator.pop(context);
+              },
+              child: const Text('Salva'),
+            ),
+          ],
+        );
+      },
+    );
+
+    // Se l'utente ha premuto il pulsante "Salva"
+    if (result == true) {
+      // Mostra un messaggio di conferma
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Scooter aggiunto correttamente'),
+        ),
+      );
+    }
+  }
+
+  // Funzione per aggiungere un nuovo scooter
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Inserisci i dati'),
+        title: const Text('I miei scooter'),
       ),
-      body: Form(
-        key: _formKey,
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: <Widget>[
-              TextFormField(
-                controller: _dataController,
-                readOnly: true,
-                decoration: InputDecoration(
-                  labelText: 'Data',
-                ),
-              ),
-              SizedBox(height: 16.0),
-              TextFormField(
-                controller: _kmController,
-                keyboardType: TextInputType.number,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Inserire un valore numerico';
-                  }
-                  return null;
-                },
-                decoration: InputDecoration(
-                  labelText: 'KM',
-                ),
-              ),
-              SizedBox(height: 16.0),
-              TextFormField(
-                controller: _litriBenzinaController,
-                keyboardType: TextInputType.number,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Inserire un valore numerico';
-                  }
-                  return null;
-                },
-                decoration: InputDecoration(
-                  labelText: 'Litri benzina',
-                ),
-              ),
-              SizedBox(height: 16.0),
-              TextFormField(
-                controller: _litriOlioController,
-                keyboardType: TextInputType.number,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Inserire un valore numerico';
-                  }
-                  return null;
-                },
-                decoration: InputDecoration(
-                  labelText: 'Litri olio',
-                ),
-              ),
-              SizedBox(height: 32.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        // Salvare i dati nel database Hive
-                        final formDataBox = Hive.box<FormDataRifornimento>(
-                            'formDataRifornimento');
-                        formDataBox.putAt(
-                            0,
-                            FormDataRifornimento(
-                              data: _dataController.text,
-                              km: int.parse(_kmController.text),
-                              litriBenzina:
-                                  double.parse(_litriBenzinaController.text),
-                              litriOlio:
-                                  double.parse(_litriOlioController.text),
-                            ));
-                        print('Dati salvati nel database Hive');
+      body: _scooters.isEmpty
+          ? _buildEmptyScreen()
+          : ListView.builder(
+              itemCount: _scooters.length,
+              itemBuilder: (context, index) {
+                final scooter = _scooters[index];
+                //return _buildScooterListItem(scooter);
+                return _buildEmptyScreen();
+              },
+            ),
+    );
+  }
 
-                        // Opzionale: pulire il form dopo il salvataggio
-                        _formKey.currentState!.reset();
-                      }
-                    },
-                    child: Text('Inserisci'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      _formKey.currentState!.reset();
-                    },
-                    child: Text('Reset'),
-                  ),
-                ],
-              ),
-            ],
+  _buildEmptyScreen() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Non hai configurato alcun mezzo'),
+          ElevatedButton(
+            onPressed: () => _addScooter(),
+            child: const Text('Inserisci'),
           ),
-        ),
+        ],
       ),
     );
+  }
+
+  // Widget _buildScooterListItem(Scooter scooter) {}
+
+  @override
+  void dispose() {
+    Hive.box('scooter').close();
+    super.dispose();
   }
 }
--- rifornimento/test/widget_test.dart
+++ rifornimento/test/widget_test.dart
@@ -8,8 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:rifornimento/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
@@ -27,4 +25,8 @@ void main() {
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
+}
+
+class MyApp {
+  const MyApp();
 }
