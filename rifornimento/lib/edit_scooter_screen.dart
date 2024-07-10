0b121068e6182f5063a44a8bc099e687a9a16e93
Adriano <adriano.abbruzzese@gmail.com>
2024-07-10 22:56:02 +0200

    ancora non funziona

--- rifornimento/lib/edit_scooter_screen.dart
+++ rifornimento/lib/edit_scooter_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:rifornimento/models/scooter.dart';
 
 class EditScooterScreen extends StatefulWidget {
@@ -11,16 +12,25 @@ class EditScooterScreen extends StatefulWidget {
 }
 
 class _EditScooterScreenState extends State<EditScooterScreen> {
-  final _nomeController = TextEditingController(text: "this.scooter");
+  late final _nomeController = TextEditingController(text: widget.scooter.nome);
+  late final _idController =
+      TextEditingController(text: widget.scooter.id.toString());
+
+  get editedScooter => null;
+
+  get box => null;
 
   @override
   void dispose() {
     _nomeController.dispose();
+    _idController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    Box box = loadData();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Modifica scooter'),
@@ -30,15 +40,34 @@ class _EditScooterScreenState extends State<EditScooterScreen> {
         child: Column(
           children: [
             TextField(
-              controller: _nomeController,
+              decoration: const InputDecoration(labelText: 'ID: '),
+              controller: _idController,
+            ),
+            const SizedBox(height: 20),
+            TextField(
               decoration: const InputDecoration(labelText: 'Nome'),
+              controller: _nomeController,
             ),
             const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
+                //box = Hive.box<Scooter>('scooter');
+                int parsedId = int.parse(_idController.text);
                 final editedScooter =
-                    Scooter(id: widget.scooter.id, nome: _nomeController.text);
-                Navigator.pop(context, editedScooter);
+                    Scooter(id: parsedId, nome: _nomeController.text);
+                try {
+                  box.put(editedScooter.id, editedScooter);
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Aggiornamento completato!')),
+                  );
+                  Navigator.pop(context);
+                } catch (error) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                        content:
+                            Text('Errore durante l\'aggiornamento: $error')),
+                  );
+                }
               },
               child: const Text('Salva'),
             ),
@@ -46,5 +75,13 @@ class _EditScooterScreenState extends State<EditScooterScreen> {
         ),
       ),
     );
+  }
+
+  Future<Box> loadData() async {
+    Hive.initFlutter();
+    await Hive.openBox('idCounter');
+    await Hive.openBox('scooter');
+
+    return Hive.box('scooter');
   }
 }
